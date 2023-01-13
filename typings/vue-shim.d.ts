b40ced1feda38bee062cd3c47ed788607cc60452
liuxin0207 <1140040227@qq.com>
2023-01-13 13:58:47 +0800

    feat: 项目基本搭建

---
+++ typings/vue-shim.d.ts
@@ -0,0 +1,9 @@
+declare module '*.vue' {
+  // 取defineComponent的返回值，标识组件类型
+  import { defineComponent, App } from 'vue'
+  const component: ReturnType<typeof defineComponent> & {
+    install(app: App): void
+  }
+  // 导出组件类型
+  export default component
+}
