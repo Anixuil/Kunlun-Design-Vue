7a54ffcb622cda69d76bd05c2d702cf702c032ba
liuxin0207 <1140040227@qq.com>
2023-01-14 16:17:23 +0800

    feat: 环境搭建

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-interface': 'off'
-  },
-  globals: { defineOptions: 'writable' }
+  }
 }
--- website/main.ts
+++ website/main.ts
@@ -4,7 +4,6 @@ import App from './App.vue'
 
 // 引入组件库
 import KunlunDesign from 'kunlun-design-vue' // 开发阶段
-console.log(KunlunDesign)
 
 import 'theme-chalk/src/index.scss' // 开发阶段引入样式
 // 创建应用并使用组件库
