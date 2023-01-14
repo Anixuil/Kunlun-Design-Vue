3ae158413edf03cc8e325462518d3abc6bb94689
liuxin0207 <1140040227@qq.com>
2023-01-14 13:14:49 +0800

    feat: npm包的配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueMacros from 'unplugin-vue-macros/vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,23 @@ export default defineConfig({
                 vue: vue()
             }
         })
-    ]
+    ],
+    build: {
+        outDir: 'lib',
+        lib: {
+            entry: resolve(__dirname, './packages/index.ts'),
+            name: 'Kunlun-Design',
+            fileName: 'kunlun-design-vue'
+        },
+        rollupOptions: {
+            //确保外部化处理那些你不想打包进库的依赖
+            external: ['vue'],
+            output: {
+                //在umd构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    vue: 'vue'
+                }
+            }
+        }
+    }
 })
