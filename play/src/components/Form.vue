190e296571b473b21013f08f655958e0bb05ef0b
zea <312770690@qq.com>
2023-02-06 17:30:21 +0800

    docs: 优化form文档

--- play/src/components/Form.vue
+++ play/src/components/Form.vue
@@ -1,14 +1,50 @@
 <template>
     <h1>Form</h1>
-    <kl-form :model="{}">
-        <kl-input placeholder="请输入账号"></kl-input>
-        <kl-input type="password" placeholder="请输入密码"></kl-input>
-        <kl-input disabled placeholder="禁用输入框" />
-    </kl-form>
+    <div class="form">
+        <kl-form
+            :model="{
+                labelWidth: '100px'
+            }"
+        >
+            <kl-form-item label="username">
+                <kl-input placeholder="请输入账号" v-model="form.name" :clearable="true"></kl-input>
+            </kl-form-item>
+            <kl-form-item label="password">
+                <kl-input
+                    type="password"
+                    placeholder="请输入密码"
+                    v-model="form.password"
+                    showPassword
+                ></kl-input>
+            </kl-form-item>
+            <kl-form-item label="disabled">
+                <kl-input disabled placeholder="禁用输入框" />
+            </kl-form-item>
+            <kl-form-item>
+                <kl-button type="primary" @click="onSubmit">submit</kl-button>
+                <kl-button>Create</kl-button>
+            </kl-form-item>
+        </kl-form>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { KlForm, KlInput } from 'kunlun-design'
+import { KlForm, KlInput, KlFormItem, KlButton } from 'kunlun-design'
+import { reactive } from 'vue'
+
+const form = reactive({
+    name: '',
+    password: ''
+})
+
+const onSubmit = () => {
+    console.log(JSON.parse(JSON.stringify(form)))
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.form {
+    width: 500px;
+    margin-left: 20px;
+}
+</style>
