8ddfdb1094c6520819f821fd0c24e3c38a1cd0de
zea <312770690@qq.com>
2023-02-06 16:42:19 +0800

    docs: form,input部分说明

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,60 +1,11 @@
 <script setup lang="ts">
-import { KlButton, KlForm, KlInput } from 'kunlun-design'
+import Button from './components/Button.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-    <div>
-        <h2>基础按钮</h2>
-        <kl-button>default</kl-button>
-        <kl-button type="default">default</kl-button>
-        <kl-button type="primary">primary</kl-button>
-        <kl-button type="success">success</kl-button>
-        <kl-button type="info">info</kl-button>
-        <kl-button type="warning">warning</kl-button>
-        <kl-button type="danger">danger</kl-button>
-        <h2>单色按钮</h2>
-        <kl-button plain>default</kl-button>
-        <kl-button type="default" plain>default</kl-button>
-        <kl-button type="primary" plain>primary</kl-button>
-        <kl-button type="success" plain>success</kl-button>
-        <kl-button type="info" plain>info</kl-button>
-        <kl-button type="warning" plain>warning</kl-button>
-        <kl-button type="danger" plain>danger</kl-button>
-        <h2>圆角按钮</h2>
-        <kl-button round>button</kl-button>
-        <kl-button type="default" round>button</kl-button>
-        <kl-button type="primary" round>button</kl-button>
-        <kl-button type="success" round>button</kl-button>
-        <kl-button type="info" round>button</kl-button>
-        <kl-button type="warning" round>button</kl-button>
-        <kl-button type="danger" round>button</kl-button>
-        <h2>圆形按钮</h2>
-        <kl-button circle>1</kl-button>
-        <kl-button type="default" circle>1</kl-button>
-        <kl-button type="primary" circle>1</kl-button>
-        <kl-button type="success" circle>1</kl-button>
-        <kl-button type="info" circle>1</kl-button>
-        <kl-button type="warning" circle>1</kl-button>
-        <kl-button type="danger" circle>1</kl-button>
-        <h2>按钮尺寸</h2>
-        <kl-button size="mini">button</kl-button>
-        <kl-button size="small">button</kl-button>
-        <kl-button size="normal">button</kl-button>
-        <kl-button size="large">button</kl-button>
-        <kl-button size="xlarge">button</kl-button>
-        <h2>prop属性</h2>
-        <kl-button color="red" textColor="white">button</kl-button>
-        <h2>自定义样式按钮</h2>
-        <kl-button class="self-button" plain>button</kl-button>
-        <h2>禁用按钮</h2>
-        <kl-button disabled type="primary" plain>button</kl-button>
-        <kl-button type="primary">button</kl-button>
-        <kl-form :model="{}">
-            <kl-input placeholder="请输入账号"></kl-input>
-            <kl-input type="password" placeholder="请输入密码"></kl-input>
-            <kl-input disabled placeholder="禁用输入框" />
-        </kl-form>
-    </div>
+    <Button></Button>
+    <Form></Form>
 </template>
 
 <style scoped lang="scss">
---
+++ play/src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Form</h1>
+    <kl-form :model="{}">
+        <kl-input placeholder="请输入账号"></kl-input>
+        <kl-input type="password" placeholder="请输入密码"></kl-input>
+        <kl-input disabled placeholder="禁用输入框" />
+    </kl-form>
+</template>
+
+<script setup lang="ts">
+import { KlForm, KlInput } from 'kunlun-design'
+</script>
+
+<style scoped></style>
