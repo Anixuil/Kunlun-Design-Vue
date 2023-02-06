616e7f1cf32f864a78f1e3107511302716e2b08f
zea <312770690@qq.com>
2023-02-06 17:28:55 +0800

    feat: 添加formItem组件

---
+++ packages/components/form-item/src/form-itrm.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="kl-form-item">
+        <label :style="{ width: labelWidth }" class="kl-form-item_label">{{ label }}</label>
+        <div class="kl-form-item_content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { createNamespace } from '@kunlun-design/utils'
+import { inject } from 'vue'
+
+defineOptions({
+    name: 'KlFormItem'
+})
+
+defineProps({
+    label: {
+        type: String,
+        default: ''
+    }
+})
+
+const labelWidth = inject<String>('labelWidth')
+
+const { n } = createNamespace('form-item')
+</script>
+
+<style scoped lang="scss">
+.kl-form-item {
+    margin-bottom: 25px;
+
+    .kl-form-item_label {
+        text-align: right;
+        vertical-align: middle;
+        float: left;
+        font-size: 14px;
+        color: #606266;
+        line-height: 40px;
+        padding: 0 12px 0 0;
+        box-sizing: border-box;
+    }
+
+    .kl-form-item_content {
+        line-height: 40px;
+        position: relative;
+        font-size: 14px;
+        overflow: hidden;
+    }
+}
+</style>
