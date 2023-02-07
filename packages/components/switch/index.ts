a5431fdace63e0d554d7c6337c00078231ade5c1
zea <312770690@qq.com>
2023-02-07 22:14:08 +0800

    feat: 新增switch组件

---
+++ packages/components/switch/index.ts
@@ -0,0 +1,7 @@
+export * from './src/switch.vue'
+import { withInstall } from '@kunlun-design/utils'
+import Switch from './src/switch.vue'
+
+export const KlSwitch = withInstall(Switch)
+
+export default KlSwitch
