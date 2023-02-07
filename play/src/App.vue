d07769dcd46a7be7d5dfe12b30a1d4e6ce6518e0
zea <312770690@qq.com>
2023-02-07 23:34:32 +0800

    docs: 优化文档

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Button from './components/Button.vue'
 import DatePicker from './components/DatePicker.vue'
 import Message from './components/Message.vue'
 import Button from './components/Button.vue'
@@ -10,7 +9,6 @@ import Form from './components/Form.vue'
     <Button></Button>
     <DatePicker></DatePicker>
     <Message></Message>
-    <Button></Button>
     <Form></Form>
 </template>
 
