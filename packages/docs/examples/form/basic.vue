67a1384d31b5d7b77741d2bc501d9b5def6b1cd4
zea <312770690@qq.com>
2023-02-17 04:23:16 +0800

    docs: form中文文档

---
+++ packages/docs/examples/form/basic.vue
@@ -0,0 +1,32 @@
+<template>
+    <kl-form :model="form" labelWidth="120px">
+        <kl-form-item label="username">
+            <kl-input placeholder="请输入账号" v-model="form.name" clearable></kl-input>
+        </kl-form-item>
+        <kl-form-item label="password">
+            <kl-input
+                type="password"
+                placeholder="请输入密码"
+                v-model="form.password"
+                showPassword
+            ></kl-input>
+        </kl-form-item>
+        <kl-form-item>
+            <kl-button type="primary" @click="login">login</kl-button>
+            <kl-button>Cancel</kl-button>
+        </kl-form-item>
+    </kl-form>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const form = reactive({
+    name: '',
+    password: ''
+})
+
+const login = () => {
+    console.log(JSON.parse(JSON.stringify(form)))
+}
+</script>
